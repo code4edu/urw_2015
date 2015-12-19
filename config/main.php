ed28b2973b10fc8b0cda293385c11c6b0a5b6121
unknown <admin@w12-215-1.main.sgu.ru>
2015-12-19 13:08:46 +0300

    add config for db

--- config/main.php
+++ config/main.php
@@ -7,4 +7,11 @@ return array(
 		'/user/auth/' => 'user/login',
 		'/user/\d+/' => 'user/update',
 	),
+
+	'db' => array(
+		'host' => 'sql4.freemysqlhosting.net',
+		'user' => 'sql4100371',
+		'password' => 'eeAiUuEwAv',
+		'database' => 'sql4100371',
+	)
 );
